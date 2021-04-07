bca9c94b14723ea9279f62e928c07e6104c75133
BurningTiles <BurningTiles>
2021-04-07 08:38:58 +0530

    updated

---
+++ 20210406/Solution.cpp
@@ -0,0 +1,27 @@
+/**
+ * Author  : BurningTiles
+ * Created : 2021-04-03 21:56:45
+ * Link    : BurningTiles.github.io
+ * File    : Solution.cpp
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+	int a, b; // take two integer from user.
+	cin >> a >> b;
+	cout << a+b << endl; // Print sum of two numbers.
+}
+
+signed main() {
+
+	int tt; // for test-case
+	cin >> tt;
+
+	while(tt--){ // a loop for test-case, you can also use for loop - for(int t=0; t<tt; t++) { //Your_code } 
+		solve(); // Calling function to do other work.
+	}
+
+	return 0;
+}
