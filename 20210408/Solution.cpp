15ccc69b2f374c6d03c1ae264c16de7b5b4aa7a4
BurningTiles <BurningTiles>
2021-04-08 21:52:02 +0530

    updated

---
+++ 20210408/Solution.cpp
@@ -0,0 +1,19 @@
+/**
+ * Author  : BurningTiles
+ * Created : 2021-04-03 21:56:45
+ * Link    : BurningTiles.github.io
+ * File    : Solution.cpp
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+signed main() {
+
+	string name;
+	cin >> name; // Take input from user
+	cout << "Hello World!" << endl;
+	cout << "Thank you " << name << " for visiting.";
+
+	return 0;
+}
