bd5e2640b1abaad7805c418888b3bace290aa190
BurningTiles <BurningTiles>
2021-04-11 16:01:38 +0530

    Updated

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
 table = document.getElementById("table");
-table.innerHTML =
-	"<tr><th>Date</th><th>Problem</th><th>Topic</th><th>Solution</th></tr>";
+table.innerHTML = "<tr><th>Date</th><th>Problem</th><th>Topic</th><th>Solution</th></tr>";
 
 link = "https://github.com/BurningTiles/DailyCoding/blob/main/";
 
 for (let i in data_table) {
 	let data = data_table[i];
 	let dir = link + data.dir + "/";
-	let row =
-		"<tr><td>" +
-		data.date +
-		"</td>" +
-		'<td><a href="' +
-		dir +
-		'Problem.pdf">' +
-		data.problem +
-		"</a></td>" +
-		"<td>" +
-		data.topic +
-		"</td><td>";
+	let row = "<tr><td>" + data.date + "</td>" + '<td><a href="' + dir + 'Problem.pdf">' + data.problem + "</a></td>" + "<td>" + data.topic + "</td><td>";
 	if (data.solution.length) {
 		for (let sol in data.solution) {
 			let ext = data.solution[sol];
@@ -28,31 +16,5 @@ for (let i in data_table) {
 		row += "Pending";
 	}
 	row += "</td>";
-
 	table.innerHTML += row;
-}
-{
-	/* <tr>
-					<th>Date</th>
-					<th>Problem</th>
-					<th>Topic</th>
-					<th>Solution</th>
-				</tr>
-				<tr>
-					<td>3 Apr 2021</td>
-					<td>
-						<a
-							href="https://github.com/BurningTiles/DailyCoding/blob/main/20210403/Problem.txt"
-							>First repeating character</a>
-					</td>
-					<td>Array</td>
-					<td>
-						<a
-							href="https://github.com/BurningTiles/DailyCoding/blob/main/20210403/Solution.cpp"
-							>cpp,</a>
-						<a
-							href="https://github.com/BurningTiles/DailyCoding/blob/main/20210403/Solution.py"
-							> py</a>
-					</td>
-				</tr> */
 }
