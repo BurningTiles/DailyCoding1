11bfb5e759cd15cf6d7bf05eeed1c738fd245157
BurningTiles <BurningTiles>
2021-04-16 14:15:54 +0530

    Updated

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 data_table = [
+	{date: "16 Apr 2021", dir: "20210416", topic: "Array", problem: "Cyclic rotate an araay", solution: []},
 	{date: "15 Apr 2021", dir: "20210415", topic: "Array", problem: "Union and Intersection of two Arrays", solution: []},
 	{date: "14 Apr 2021", dir: "20210414", topic: "Array", problem: "Move all negative numbers to begining", solution: ["CPP"]},
 	{date: "13 Apr 2021", dir: "20210413", topic: "Array", problem: "Sort an Array in linear time", solution: ["CPP"]},
